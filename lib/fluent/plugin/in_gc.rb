6a4e86fe52440fe9f77a3f10a270fa08883d4983
sonots <sonots@gmail.com>
2014-02-04 15:12:38 +0900

    Support log_level of Fluentd 0.10.43

--- lib/fluent/plugin/in_gc.rb
+++ lib/fluent/plugin/in_gc.rb
@@ -2,6 +2,11 @@
 class Fluent::GcInput < Fluent::Input
   Fluent::Plugin.register_input('gc', self)
 
+  # To support log_level option implemented by Fluentd v0.10.43
+  unless method_defined?(:log)
+    define_method("log") { $log }
+  end
+
   config_param :disable, :bool, :default => false
   config_param :interval, :time, :default => 5
   config_param :debug, :bool, :default => false
@@ -37,16 +42,16 @@ class Fluent::GcInput < Fluent::Input
           @last_checked = now
         end
       rescue => e
-        $log.warn "#{e.class} #{e.message} #{e.backtrace.first}"
+        log.warn "#{e.class} #{e.message} #{e.backtrace.first}"
       end
     end
   end
 
   def start_gc
-    $log.info "gc: before #{GC.stat}" if @debug # intentionally info level
+    log.info "gc: before #{GC.stat}" if @debug # intentionally info level
     disabled = GC.enable
     GC.start
     GC.disable if disabled
-    $log.info "gc: after  #{GC.stat}" if @debug
+    log.info "gc: after  #{GC.stat}" if @debug
   end
 end
